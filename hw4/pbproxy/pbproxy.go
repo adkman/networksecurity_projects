000d6e7ef4ad341b274c03c89f6c2733b126d817
Manish Adkar <madkar@cs.stonybrook.edu>
2021-04-30 20:59:34 +0000

    hw4 - commenting out the logs

--- hw4/pbproxy/pbproxy.go
+++ hw4/pbproxy/pbproxy.go
@@ -33,28 +33,28 @@ func handleConnection (clientConn net.Conn) {
     serviceData := make([]byte, 1600)
     go func() {
         for {
-            log.Println("READING FROM SERVICE")
+            //log.Println("READING FROM SERVICE")
             if nr2, err := serviceConn.Read(serviceData); err == nil {
-                log.Println("READ FROM SERVICE DONE", nr2, hex.Dump(serviceData[:nr2]))
-                nw2, err := clientConn.Write(serviceData[:nr2])
+                //log.Println("READ FROM SERVICE DONE", nr2, hex.Dump(serviceData[:nr2]))
+                _, err := clientConn.Write(serviceData[:nr2])
                 check(err)
-                log.Println("WRITE TO CLIENT DONE", nw2, hex.Dump(serviceData[:nw2]))
+                //log.Println("WRITE TO CLIENT DONE", nw2, hex.Dump(serviceData[:nw2]))
             } else {
-                log.Println("BROKE 1", err)
+                //log.Println("BROKE 1", err)
                 break
             }
         }
     }()
     clientData := make([]byte, 1600)
     for {
-        log.Println("READING FROM CLIENT")
+        //log.Println("READING FROM CLIENT")
         if nr1, err := clientConn.Read(clientData); err == nil {
-            log.Println("READ FROM CLIENT DONE", nr1, hex.Dump(clientData[:nr1]))
-            nw1, err := serviceConn.Write(clientData[:nr1])
+            //log.Println("READ FROM CLIENT DONE", nr1, hex.Dump(clientData[:nr1]))
+            _, err := serviceConn.Write(clientData[:nr1])
             check(err)
-            log.Println("WRITE TO SERVICE DONE", nw1, hex.Dump(clientData[:nw1]))
+            //log.Println("WRITE TO SERVICE DONE", nw1, hex.Dump(clientData[:nw1]))
         } else {
-            log.Println("BROKE", err)
+            //log.Println("BROKE", err)
             break
         }
     }
@@ -86,28 +86,28 @@ func readAndSend() {
     stdinData := make([]byte, 1)
     go func() {
         for {
-            log.Println("READING FROM SERVER")
+            //log.Println("READING FROM SERVER")
             if n, err := conn.Read(serverData); err == nil {
-                log.Println("READ FROM SERVER", n, hex.Dump(serverData[:n]))
+                //log.Println("READ FROM SERVER", n, hex.Dump(serverData[:n]))
                 os.Stdout.Write(serverData[:n])
-                log.Println("WRITE TO STDOUT DONE")
+                //log.Println("WRITE TO STDOUT DONE")
             } else {
-                log.Println("BROKE")
+                //log.Println("BROKE")
                 break
             }
         }
     }()
 
     for {
-        log.Println("READING FROM STDIN")
+        //log.Println("READING FROM STDIN")
         if data, err := reader.ReadByte(); err == nil {
             stdinData[0] = data
-            log.Println("READ FROM STDIN DONE", hex.Dump(stdinData))
+            //log.Println("READ FROM STDIN DONE", hex.Dump(stdinData))
             _, err := conn.Write(stdinData)
             check(err)
-            log.Println("WRITE TO SERVER DONE")
+            //log.Println("WRITE TO SERVER DONE")
         } else {
-            log.Println("BROKE 1")
+            //log.Println("BROKE 1")
             break
         }
     }
