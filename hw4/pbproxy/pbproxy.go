8bff449aab84c6ecd086d8f5832db216bb6fcc70
Manish Adkar <madkar@cs.stonybrook.edu>
2021-05-01 02:56:49 +0000

    hw4 - comments

--- hw4/pbproxy/pbproxy.go
+++ hw4/pbproxy/pbproxy.go
@@ -12,6 +12,7 @@ import (
     "crypto/cipher"
     "crypto/rand"
     "crypto/sha256"
+//    "encoding/hex"
 )
 
 var (
@@ -29,6 +30,7 @@ func check(e error) {
 
 func encrypt(plaintext []byte) []byte {
     salt := make([]byte, saltLength)
+//    log.Println("salt", len(salt), hex.Dump(salt))
     rand.Read(salt)
     aesKey := pbkdf2.Key([]byte(passwd), salt, 4096, 32, sha256.New)
 
@@ -40,7 +42,7 @@ func encrypt(plaintext []byte) []byte {
 
     nonce := make([]byte, aesgcm.NonceSize())
     rand.Read(nonce)
-    check(err)
+//    log.Println("nonce", len(nonce), hex.Dump(nonce))
 
     data := append(salt, nonce...)
     return aesgcm.Seal(data, nonce, plaintext, nil)
@@ -80,8 +82,9 @@ func handleConnection (clientConn net.Conn) {
         for {
             //log.Println("READING FROM SERVICE")
             if nr2, err := serviceConn.Read(serviceData); err == nil {
-                //log.Println("READ FROM SERVICE DONE", nr2, hex.Dump(serviceData[:nr2]))
+//                log.Println("Before", nr2, hex.Dump(serviceData[:nr2]))
                 encryptedServiceData := encrypt(serviceData[:nr2])
+//                log.Println("After", len(encryptedServiceData), hex.Dump(encryptedServiceData[:len(encryptedServiceData)]))
                 _, err := clientConn.Write(encryptedServiceData)
                 check(err)
                 //log.Println("WRITE TO CLIENT DONE", nw2, hex.Dump(serviceData[:nw2]))
@@ -96,9 +99,7 @@ func handleConnection (clientConn net.Conn) {
         //log.Println("READING FROM CLIENT")
         if nr1, err := clientConn.Read(clientData); err == nil {
             //log.Println("READ FROM CLIENT DONE", nr1, hex.Dump(clientData[:nr1]))
-            log.Println("GOT", nr1)
             decryptedClientData := decrypt(clientData[:nr1])
-            log.Println("SENDING", len(decryptedClientData))
             _, err := serviceConn.Write(decryptedClientData)
             check(err)
             //log.Println("WRITE TO SERVICE DONE", nw1, hex.Dump(clientData[:nw1]))
